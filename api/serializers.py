644c58ea5053bbb3403b7c580fbc714e9ec823e0
Nathaniel Fredericks <fna371858@gmail.com>
2020-12-23 16:35:54 -0400

    Add fahrenheit to API

--- api/serializers.py
+++ api/serializers.py
@@ -3,6 +3,16 @@ from .models import Temperature
 
 
 class TemperatureSerializer(serializers.ModelSerializer):
+    temperature_c = serializers.SerializerMethodField()
+    temperature_f = serializers.SerializerMethodField()
+
     class Meta:
         model = Temperature
-        fields = ['id', 'temperature', 'date']
+        fields = ['id', 'temperature_c', 'temperature_f', 'date']
+
+    def get_temperature_c(self, obj):
+        return obj.temperature
+
+    def get_temperature_f(self, obj):
+        return (obj.temperature * 1.8) + 32
+
--- chart/static/chart/chart.js
+++ chart/static/chart/chart.js
@@ -18,28 +18,34 @@ async function generateDataTable() {
     const dataTable = new google.visualization.DataTable();
 
     // add columns
-    dataTable.addColumn('date', 'date');
-    dataTable.addColumn('number', 'temperature');
+    dataTable.addColumn('date', 'Date');
+    dataTable.addColumn('number', 'Temperature');
 
     // fetch temp data from api
     const data = await fetchData();
 
     // map over rows and reformat for data table
     const rows = data.map(function (temp) {
-        // [parsed date, temp float]
-        return [new Date(temp.date), temp.temperature];
+        // [parsed date, temp celsius float]
+        return [new Date(temp.date), temp.temperature_c];
     });
 
     // add rows to data table
     dataTable.addRows(rows);
 
     // add celsius to tooltip
-    const formatter = new google.visualization.NumberFormat({
+    const tempFormatter = new google.visualization.NumberFormat({
         suffix: 'ºC',
         fractionDigits: 0
     });
 
-    formatter.format(dataTable, 1);
+    // add time & date to tooltip
+    const dateFormatter = new google.visualization.DateFormat({
+        pattern: "MMM d, yyyy, h:mm aa"
+    });
+
+    tempFormatter.format(dataTable, 1);
+    dateFormatter.format(dataTable, 0);
 
     return dataTable;
 }
