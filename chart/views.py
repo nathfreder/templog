0d1bf21f5aafd6391cab04fcee88536f6c386bb4
Nathaniel Fredericks <fna371858@gmail.com>
2021-01-11 20:05:35 -0400

    Show current temperature

--- chart/views.py
+++ chart/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from api.models import Temperature
 
 
 def index(request):
-    return render(request, 'chart/index.html')
+    temperature = Temperature.objects.latest('date')
+    return render(request, 'chart/index.html', {'temperature': round(temperature.temperature)})
