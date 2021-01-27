b620a770819ede3fcf783f13e26c2397c132d08e
Nathaniel Fredericks <fna371858@gmail.com>
2021-01-27 01:00:35 -0400

    add date range selector

--- chart/static/chart/chart.js
+++ chart/static/chart/chart.js
@@ -4,9 +4,18 @@ google.charts.load('current', {packages: ['corechart', 'line']})
 /**
  * Fetch temperatures from API
  * @return {Promise<any>}
+ * @param {string} before
+ * @param {string} after
  */
-async function fetchData() {
-    const response = await fetch('/api/temps');
+async function fetchData(
+    // default is current select value
+    before = getBeforeAfter(document.getElementById('date-range').value).before,
+    after = getBeforeAfter(document.getElementById('date-range').value).after
+) {
+    const response = await fetch('/api/temps/?' + new URLSearchParams({
+        date_before: before,
+        date_after: after,
+    }));
     return await response.json()
 }
 
@@ -14,7 +23,7 @@ async function fetchData() {
  * Generate data table from data
  * @return {Promise<google.visualization.DataTable>}
  */
-async function generateDataTable() {
+async function generateDataTable(before, after) {
     const dataTable = new google.visualization.DataTable();
 
     // add columns
@@ -22,7 +31,7 @@ async function generateDataTable() {
     dataTable.addColumn('number', 'Temperature');
 
     // fetch temp data from api
-    const data = await fetchData();
+    const data = await fetchData(before, after);
 
     // map over rows and reformat for data table
     const rows = data.map(function (temp) {
@@ -54,9 +63,9 @@ async function generateDataTable() {
  * Draw dashboard
  * @return {Promise<void>}
  */
-async function drawDashboard() {
+async function drawDashboard(before, after) {
     // get data table
-    const data = await generateDataTable();
+    const data = await generateDataTable(before, after);
 
     // create line chart
     const chart = new google.visualization.LineChart(document.getElementById('chart'));
@@ -74,4 +83,13 @@ async function drawDashboard() {
                 position: 'none'
             }
     });
+
+    // get date range
+    const select = document.getElementById('date-range');
+
+    // redraw chart when date range changes
+    select.onchange = function () {
+        const { before, after } = getBeforeAfter(select.value);
+        drawDashboard(before, after);
+    }
 }
---
+++ chart/static/chart/utils.js
@@ -0,0 +1,33 @@
+/**
+ * Get number of days ago in ISO format
+ * @param {number} days
+ */
+function daysAgo(days) {
+    const date = new Date();
+    date.setDate(date.getDate() - days);
+    return date.toISOString();
+}
+
+/**
+ * Get before and after dates from select value
+ * @param {string} select
+ */
+function getBeforeAfter(select) {
+    let before = '';
+    let after = '';
+    switch (select) {
+        case 'day':
+            after = daysAgo(1);
+            break;
+        case 'week':
+            after = daysAgo(7);
+            break;
+        case '2weeks':
+            after = daysAgo(14);
+            break;
+        case 'month':
+            after = daysAgo(1);
+            break;
+    }
+    return { before, after }
+}
