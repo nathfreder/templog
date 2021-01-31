60d6cf2e29bfb31ce1d4d6bffff85facaa2a7f7b
Nathaniel Fredericks <fna371858@gmail.com>
2021-01-31 00:12:46 -0400

    round temperature to whole number
    fixes inconsistencies in template and makes chart more accurate

--- api/serializers.py
+++ api/serializers.py
@@ -2,7 +2,24 @@ from rest_framework import serializers
 from .models import Temperature
 
 
+def round_temperature(temperature):
+    """
+    Round temperature to ones place
+    :rtype: int
+    """
+    return round(temperature)
+
+
+class TemperatureField(serializers.Field):
+    def to_representation(self, value):
+        return round_temperature(value)
+
+    def to_internal_value(self, data):
+        return data
+
+
 class TemperatureSerializer(serializers.ModelSerializer):
+    temperature = TemperatureField()
     temperature_f = serializers.SerializerMethodField()
 
     class Meta:
@@ -10,5 +27,4 @@ class TemperatureSerializer(serializers.ModelSerializer):
         fields = ['id', 'temperature', 'temperature_f', 'date']
 
     def get_temperature_f(self, obj):
-        return (obj.temperature * 1.8) + 32
-
+        return round_temperature((obj.temperature * 1.8) + 32)
